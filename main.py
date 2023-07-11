ce634e4467b4e533928d394c206e6f8635b82e92
farzanehnia <86298252+farzanehnia@users.noreply.github.com>
2023-07-11 20:43:04 +0000

    data

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'series_1':[1,2,3,4,5],
+  'series_2':[6,7,8,9,10]
+}
+
+df = pd.DataFrame(data)
 
 
 st.title('Our first streamlit')
 st.subheader('Intro to dashboard with python')
 st.write('''Benvenuti in''')
+
+st.write(df)
